0ef8f82c1a59b930ce8aed33e2429dc39c0f72e5
SamGombs20 <gombajosh18@gmail.com>
2025-03-12 13:30:58 +0300

    Update:The model is working in the API

--- code/model/input.py
+++ code/model/input.py
@@ -1,19 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Literal
 
 class InputModel(BaseModel):
-    Gender: str
-    Age: int
-    Height: float
-    Weight: float
-    family_history_with_overweight:str
-    FAVC: str
+    Gender: Literal["Male", "Female"]
+    Age: int = Field(...,gt=0, description="Age must be greater than 0")
+    Height: float = Field(...,gt=0,description="Height must be greater than 0")
+    Weight: float = Field(..., gt=0, description="Weight must be greater than 0")
+    family_history_with_overweight:Literal["yes", "no"]
+    FAVC: Literal["yes", "no"]
     FCVC: float
-    NCP: float
-    CAEC: str
-    SMOKE: str
-    CH2O: float
-    SCC: str
-    FAF: float
+    NCP: float = Field(..., ge=0, description="Number of main meals per day")
+    CAEC: Literal["no", "Sometimes", "Frequently", "Always"]
+    SMOKE: Literal["yes", "no"]
+    CH2O: float = Field(..., ge=0, description="Water intake in liters")
+    SCC: Literal["yes", "no"]
+    FAF: float = Field(..., ge=0, description="Physical activity level")
     TUE: float
-    CALC: str
-    MTRANS: str
+    CALC: Literal["no", "Sometimes", "Frequently", "Always"]
+    MTRANS: Literal["Public_Transportation", "Automobile", "Walking", "Motorbike", "Bike"]
